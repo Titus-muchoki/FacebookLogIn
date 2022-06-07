2566d58deff77c0a9336fbe795a71e97c08f5f87
kajela <titoyut56@gmail.com>
2022-06-07 01:33:16 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,31 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
+    static void checkAge(int age){
+        if (age < 18) {
+            System.out.println("your not old enough you cant join");
+        } else if (age >=18) {
+            System.out.println("your old enought enter your account detail.");
+
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = " enter a name";
+        int age = 18;
+        String email = "verified";
+        String logIn = "user input";
+
+        //user interface.
+        System.out.println("*****************");
+        System.out.println("");
+        System.out.println("Enter your name:");
+
+        if (age == 18 || email ==name) {
+            System.out.println("the user is verified: "+name);
+        }else {
+            System.out.println("the account is unknown");
+        }
+       // System.out.println("Hello world!");
+        checkAge(10);
     }
 }
